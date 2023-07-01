9361fab474605ba98d6b5b6507a9b201fe2cc178
ziomiwang <ziomiwang@gmail.com>
2023-07-01 21:36:09 +0200

    init

---
+++ src/main/java/com/example/placesandevents/PlacesAndEventsApplication.java
@@ -0,0 +1,13 @@
+package com.example.placesandevents;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PlacesAndEventsApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PlacesAndEventsApplication.class, args);
+    }
+
+}
